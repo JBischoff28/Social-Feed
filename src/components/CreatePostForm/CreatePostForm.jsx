9319b75375b605c3e48c27604975838ef4f4ea22
Joshua Bischoff <joshuabischoff1@gmail.com>
2023-01-22 17:09:00 -0500

    Developed Core Functionality For CreatePostForm Component.

--- src/components/CreatePostForm/CreatePostForm.jsx
+++ src/components/CreatePostForm/CreatePostForm.jsx
@@ -1,12 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const CreatePostForm = (props) => {
+
+    const [name, setName] = useState('');
+    const [postContent, setPostContent] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newPost = {
+            name: name,
+            post: postContent
+        };
+        console.log(newPost);
+    }
+
     return ( 
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Name</label>
-            <input type="text" />
+            <input type="text" value={name} onChange={(event) => setName(event.target.value)}/>
             <label>Post</label>
-            <input type="text" />
+            <input type="text" value={postContent} onChange={(event) => setPostContent(event.target.value)}/>
+            <button type='submit'>CREATE</button>
         </form>
      );
 }
