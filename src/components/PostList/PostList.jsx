2096a728ccad18ce59645623b29914880f9b085a
Joshua Bischoff <joshuabischoff1@gmail.com>
2023-01-26 11:41:00 -0500

    Cloned Project To Different Device, Installed Bootstrap In Order To Style The App. Created A Parameter So That Every Entry Has Its Own Unique ID.

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -3,9 +3,9 @@ import Post from "../Post/Post";
 const PostList = (props) => {
     return ( 
         <ol style={{listStyle: 'none'}}>
-            {props.appEntries.map((entry) => 
+            {props.appEntries.map((entry, index) => 
                 (
-                <div key={1}>
+                <div key={index}>
                     <Post entry={entry}/>
                 </div>
                 )
