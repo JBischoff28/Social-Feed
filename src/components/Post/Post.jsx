b5fc7016850c22b47d59e3ec6f85973c5c78dff5
Joshua Bischoff <joshuabischoff1@gmail.com>
2023-01-27 20:20:16 -0500

    Tried to Create Like And Dislike Feature. Erased Progress On The Code Involved To Start Again From Scratch.

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,9 +1,11 @@
 
 const Post = (props) => {
+
     return ( 
         <div className="postContainer">
             <h2>{props.entry.name}</h2>
             <p>{props.entry.post}</p>
+            <br />
             <hr />
         </div>
      );
